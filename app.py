fbc5e2c035770bb0022ca4ecede33c3cd0f677ab
Lauren Schmidt <laurenschmidt@gwu.edu>
2024-05-20 19:05:53 -0400

    Fixed after consult, still non functional

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+project_types = {
+        
+
+}
+
+def validate_form(form_response):
+    return
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
